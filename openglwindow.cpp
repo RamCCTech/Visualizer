7763bc3d05d97d3a1b51cf3d290d22bc3326dc2c
RamCCTech <149322355+RamCCTech@users.noreply.github.com>
2023-12-23 10:29:19 +0530

    Update openglwindow.cpp

--- openglwindow.cpp
+++ openglwindow.cpp
@@ -1,19 +1,16 @@
 #include "stdafx.h"
 #include "OpenGLWindow.h"
-
 #include <QOpenGLContext>
 #include <QOpenGLPaintDevice>
 #include <QOpenGLShaderProgram>
 #include <QPainter>
-
 #include "SutherlandCohen.h"
 #include "SutherlandHodgman.h"
 #include "HermiteCurve.h"
 #include "BezierCurve.h"
 #include "BSplineCurve.h"
 
-OpenGLWindow::OpenGLWindow(const QColor& background, QWidget* parent) :
-    mBackground(background), mClippingPolygon({})
+OpenGLWindow::OpenGLWindow(const QColor& background, QWidget* parent) : mBackground(background)
 {
     setParent(parent);
     setMinimumSize(300, 250);
@@ -37,63 +34,6 @@ void OpenGLWindow::reset()
     doneCurrent();
 
     QObject::disconnect(mContextWatchConnection);
-}
-
-void OpenGLWindow::paintGL()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    mProgram->bind();
-
-    setupMatrix();
-
-    QVector<GLfloat> vertices;
-    QVector<GLfloat> colors;
-
-    addClippingPolygonVertices(vertices, colors);
-    addPolygonsVertices(vertices, colors);
-    addLinesVertices(vertices, colors);
-
-    drawVertices(vertices, colors);
-
-    mProgram->release();
-}
-
-void OpenGLWindow::clipPolygons()
-{
-    for (int i = 0; i < mPolygons.size(); i++) {
-        SutherlandHodgman sh(mClippingPolygon, mPolygons.at(i));
-        mPolygons.at(i) = sh.getClippedPolygon();
-    }
-    emit shapesUpdated();
-}
-
-void OpenGLWindow::clipLines()
-{
-    for (int i = 0; i < mLines.size(); i++) {
-        SutherlandCohen sc(mClippingPolygon, mLines[i]);
-        mLines[i] = sc.getClippedLine();
-    }
-    emit shapesUpdated();
-}
-
-void OpenGLWindow::addClippingPolygon(Shape* s)
-{
-    mClippingPolygon = *s;
-    emit shapesUpdated();
-}
-
-void OpenGLWindow::addPolygons(Shape* s)
-{
-    mPolygons.push_back(*s);
-    emit shapesUpdated();
-}
-
-void OpenGLWindow::addLines(std::vector<Line> lines)
-{
-    for (Line l : lines)
-        mLines.push_back(l);
-    emit shapesUpdated();
 }
 
 void OpenGLWindow::initializeGL()
@@ -120,6 +60,7 @@ void OpenGLWindow::initializeGL()
     mProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
     mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
     mProgram->link();
+
     m_posAttr = mProgram->attributeLocation("posAttr");
     Q_ASSERT(m_posAttr != -1);
     m_colAttr = mProgram->attributeLocation("colAttr");
@@ -133,43 +74,26 @@ void OpenGLWindow::setupMatrix()
     QMatrix4x4 matrix;
     matrix.ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.1f, 100.0f);
     matrix.translate(0, 0, -2);
+    matrix.rotate(rotationAngle);
 
     mProgram->setUniformValue(m_matrixUniform, matrix);
 }
 
-void OpenGLWindow::addClippingPolygonVertices(QVector<GLfloat>& vertices, QVector<GLfloat>& colors)
+void OpenGLWindow::paintGL()
 {
-    addShapeVertices(mClippingPolygon, vertices, colors, 1.0f, 1.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    mProgram->bind();
+    setupMatrix();
+
+    drawVertices(mVertices, mColors);
+
+    mProgram->release();
 }
 
-void OpenGLWindow::addPolygonsVertices(QVector<GLfloat>& vertices, QVector<GLfloat>& colors)
+void OpenGLWindow::updateShape(QVector<GLfloat>& vertices, QVector<GLfloat>& colors)
 {
-    for (int i = 0; i < mPolygons.size(); i++) {
-        addShapeVertices(mPolygons.at(i), vertices, colors, 1.0f, 1.0f, 1.0f);
-    }
-}
-
-void OpenGLWindow::addLinesVertices(QVector<GLfloat>& vertices, QVector<GLfloat>& colors)
-{
-    addShapeVertices(mLines, vertices, colors, 0.0f, 1.0f, 1.0f);
-}
-
-void OpenGLWindow::addShapeVertices(Shape shape, QVector<GLfloat>& vertices, QVector<GLfloat>& colors, float red, float green, float blue)
-{
-    std::vector<Line> lines = shape.getShape();
-    for (int j = 0; j < lines.size(); j++) {
-        vertices << lines.at(j).p1().x() << lines.at(j).p1().y();
-        vertices << lines.at(j).p2().x() << lines.at(j).p2().y();
-        colors << red << green << blue;
-        colors << red << green << blue;
-    }
-}
-
-void OpenGLWindow::addCurveLines(const std::vector<Point3D>& points)
-{
-    for (int i = 0; i < points.size() - 1; i++) {
-        mLines.push_back(Line(points[i], points[i + 1]));
-    }
+    mVertices = vertices;
+    mColors = colors;
     emit shapesUpdated();
 }
 
@@ -185,4 +109,20 @@ void OpenGLWindow::drawVertices(const QVector<GLfloat>& vertices, const QVector<
 
     glDisableVertexAttribArray(m_colAttr);
     glDisableVertexAttribArray(m_posAttr);
+}
+
+void OpenGLWindow::mouseMoveEvent(QMouseEvent* event)
+{
+    int dx = event->x() - lastPos.x();
+    int dy = event->y() - lastPos.y();
+
+    if (event->buttons() & Qt::LeftButton)
+    {
+        QQuaternion rotX = QQuaternion::fromAxisAndAngle(0.0f, 1.0f, 0.0f, 0.5f * dx);
+        QQuaternion rotY = QQuaternion::fromAxisAndAngle(1.0f, 0.0f, 0.0f, 0.5f * dy);
+
+        rotationAngle = rotX * rotY * rotationAngle;
+        update();
+    }
+    lastPos = event->pos();
 }
